358f4a99e6aa0c9dd848ea6880286be641536ed4
Leo Dion <leogdion@brightdigit.com>
2015-10-31 13:28:27 -0400

    removed ipaddressrange builder

--- FlockStation/IpAddress.swift
+++ FlockStation/IpAddress.swift
@@ -145,57 +145,6 @@ public enum IpAddressFamily {
   }
 }
 
-public protocol IpAddressRangeBuildable {
-  func build(ifaddrs_ptr : UnsafeMutablePointer<ifaddrs>) -> IpAddressRangable?
-}
-
-public class IpAddressRangeBuilder : IpAddressRangeBuildable {
-  internal static let _builder = IpAddressRangeBuilder()
-  
-  public func build(ifaddrs_ptr : UnsafeMutablePointer<ifaddrs>) -> IpAddressRangable? {
-    let flags = Int32(ifaddrs_ptr.memory.ifa_flags)
-    var sa_addr = ifaddrs_ptr.memory.ifa_addr.memory
-    let addr = UnsafeMutablePointer<sockaddr_in>(ifaddrs_ptr.memory.ifa_addr)
-    
-    // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
-    if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
-      
-      let netmask = UnsafeMutablePointer<sockaddr_in>(ifaddrs_ptr.memory.ifa_netmask)
-      //let addrStrLen:Int32
-      //let separator:String
-      let family: IpAddressFamily = IpAddressFamily(sin_family: addr.memory.sin_family)
-      var ipAddressStringCharArray:[CChar]
-      var netmaskStringCharArray:[CChar]
-      
-      guard let builder = family.builder(), length = family.length else {
-        return nil
-      }
-      
-      
-      
-      ipAddressStringCharArray = [CChar](count:length, repeatedValue: 0)
-      inet_ntop(Int32(addr.memory.sin_family), &(addr.memory.sin_addr), &ipAddressStringCharArray, socklen_t(length))
-      
-      netmaskStringCharArray = [CChar](count:length, repeatedValue: 0)
-      inet_ntop(Int32(netmask.memory.sin_family), &(netmask.memory.sin_addr), &netmaskStringCharArray, socklen_t(length))
-      
-      guard let netmaskString = String.fromCString(&netmaskStringCharArray), let ipAddress = String.fromCString(&ipAddressStringCharArray)  else {
-        return nil
-      }
-      
-      //let nmComponents = netmaskString?.componentsSeparatedByString(separator)
-      //let ipComponents = ipAddress?.componentsSeparatedByString(separator)
-      
-      if let ipAddress = builder.build(ipAddress), netmaskAddress = builder.build(netmaskString) {
-        return IpAddressRangeFactory.factory.range(fromIpAddress: ipAddress, netmask: netmaskAddress)
-      } else {
-        return nil
-      }
-    }
-    return nil
-  }
-}
-
 extension String {
   public static func separator(forIpAddressFamily ipAddressFamily: IpAddressFamily) -> String? {
     switch ipAddressFamily {
@@ -207,12 +156,6 @@ extension String {
       return nil
     }
     
-  }
-}
-
-extension IpAddressRangeBuildable {
-  static var builder:IpAddressRangeBuildable {
-    return IpAddressRangeBuilder._builder
   }
 }
 
